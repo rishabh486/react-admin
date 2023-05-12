c1ba446dbb665b470a0a2f3be0f10fcf0d8899c9
rishabh486 <76812565+rishabh486@users.noreply.github.com>
2023-05-12 22:06:13 +0530

    sidebar-added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,42 @@
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import DashBoard from "./scenes/dashboard";
+import Sidebar from "./scenes/global/Sidebar";
+import SidebarC from "./scenes/global/Sidebar";
+// import Team from "./scenes/team";
+// import Contacts from "./scenes/contacts";
+// import Invoice from "./scenes/invoice";
+// import Team from "./scenes/team";
+// import Bar from "./scenes/bar";
+// import Sidebar from "./scenes/global/Sidebar";
+// import Sidebar from "./scenes/global/Sidebar";
+// import Sidebar from "./scenes/global/Sidebar";
+
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-      <h1>hello</h1>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <SidebarC />
+          <main className="content">
+            <Topbar />
+
+            <Routes>
+              <Route path="/" element={<DashBoard />} />
+              {/* <Route path="/team" element ={<Team/>}/>
+              <Route path="/bar" element ={<Bar/>}/>
+              <Route path="/contacts" element ={<Contacts/>}/>
+              <Route path="/invoice" element ={<Invoice/>}/> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,53 @@
+import { Box, Icon, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsModeOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+const Topbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+  return (
+    <div>
+      <Box display="flex" justify-content="space-between" p={2}>
+        {/* search component */}
+        <Box
+          display="flex"
+          backgroundColor={colors.grey[400]}
+          borderRadius="3px"
+        >
+          <InputBase sx={{ ml: 2, flex: 1 }} placeholder="Search" />
+          <IconButton type="button" sx={{ p: 1 }}>
+            <SearchIcon />
+          </IconButton>
+        </Box>
+        {/* icons */}
+        <Box dsiplay="flex">
+          <IconButton onClick={colorMode.toggleColorMode}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlinedIcon />
+            ) : (
+              <LightModeOutlinedIcon />
+            )}
+          </IconButton>
+          <IconButton>
+            <NotificationsModeOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <PersonOutlinedIcon />
+          </IconButton>
+        </Box>
+      </Box>
+    </div>
+  );
+};
+
+export default Topbar;
